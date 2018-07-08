c9dac889f8d40737dca7c3d965d19855d975d5c1
jiujiang <102199212@qq.com>
2018-07-08 12:34:51 +0800

    开门红工程

---
+++ src/main/java/com/tango/skills/tangoskill/TangoskillApplication.java
@@ -0,0 +1,16 @@
+package com.tango.skills.tangoskill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * 示例应用  总结过去的经验
+ */
+@SpringBootApplication
+public class TangoskillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TangoskillApplication.class, args);
+	}
+}
